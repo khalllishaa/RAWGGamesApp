ce9f30179876a8d8ffcd4c3b30c89c0b8e8409f2
khalisha kaylanasywa <kkaylanasywa>
2025-09-10 11:26:18 +0700

    fix: [KK] fixing all page

--- app/src/main/java/com/example/rawggamesapp/model/Game.kt
+++ app/src/main/java/com/example/rawggamesapp/model/Game.kt
@@ -1,10 +1,26 @@
 package com.example.rawggamesapp.model
 import com.google.gson.annotations.SerializedName
 
+//data class Game(
+//    val id: Int,
+//    val name: String,
+//    val released: String?,
+//    val rating: Double,
+//    val rating_top: Int,
+//    val ratings_count: Int,
+//    val description: String? = null,
+//    @SerializedName("background_image") val backgroundImage: String?
+//)
+
 data class Game(
     val id: Int,
     val name: String,
-    val released: String?,
-    val description: String?,
+    val released: String? = "Unknown",
+//    val backgroundImage: String? = null,
+    val description: String? = null,
+    val rating: Double = 0.0,
+    val ratingTop: Int = 0,
+    val ratingsCount: Int = 0,
     @SerializedName("background_image") val backgroundImage: String?
 )
+
--- app/src/main/java/com/example/rawggamesapp/model/GameResponse.kt
+++ app/src/main/java/com/example/rawggamesapp/model/GameResponse.kt
@@ -1,5 +1,24 @@
 package com.example.rawggamesapp.model
 
+import com.google.gson.annotations.SerializedName
+
 data class GameResponse(
-    val results: List<Game>
+    val id: Int,
+    val name: String,
+    val released: String?,
+    @SerializedName("background_image")
+    val backgroundImage: String?,
+    val rating: Double,
+    @SerializedName("rating_top")
+    val ratingTop: Int,
+    @SerializedName("ratings_count")
+    val ratingsCount: Int,
+    val description: String?
+)
+
+data class GamesListResponse(
+    val count: Int,
+    val next: String?,
+    val previous: String?,
+    val results: List<GameResponse>
 )
